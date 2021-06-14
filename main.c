c2ef93e0dde2356c29b353334b5a857219ba5ccb
Rayhan Rusyd <antianobl@gmail.com>
2021-06-14 15:54:41 +0700

    FINAL

--- main.c
+++ main.c
@@ -1,188 +1,153 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct data{
-    char arrusername[20];
-    char arrpassword[20];
-}datauser[100];
+struct userAccount
+{
+    char username[50];
+    char password[50];
+};
 
-int menu;
+struct club
+{
+    char name[50];
+    int trophy;
+};
 
-void masuk(){
+void createAccount(void);
+void login(void);
+void menu(void);
 
-    int i,end=0;
-    char buff[20],tryusername[20],trypassword[20];
-    FILE *datausername;
-    FILE *datapassword;
+int main(void)
+{
+    int n;
 
-    datausername=fopen("username.txt","r");
-    datapassword=fopen("password.txt","r");
+    printf("Type '1' for register.\nType '2' for login.\n");
+    scanf("%d", &n); fflush(stdin);
 
-    i=0;
-    while(fgets(buff,sizeof(buff),datausername)!=NULL){
-        strcpy(datauser[i].arrusername,buff);
-        i++;
-        end++;
-    }
-    fclose(datausername);
-
-    i=0;
-    while(fgets(buff,sizeof(buff),datapassword)!=NULL){
-        strcpy(datauser[i].arrpassword,buff);
-        i++;
-    }
-    fclose(datapassword);
-
-    printf("| = = = = = | Log in | = = = = = |\n");
-    printf("username: ");
-    fgets(tryusername,sizeof(tryusername),stdin);
-    printf("password: ");
-    fgets(trypassword,sizeof(trypassword),stdin);
-    system("cls");
-
-    i=0;
-    while(i<=end+1){
-        if(i>end){
-            system("cls");
-            printf("(ops sorry your username not found\n");
-            printf("1. Try again\n");
-            printf("2. Back To menu\n");
-            printf("\nSelect menu: ");
-            scanf("%d",&menu);
-            fflush(stdin);
-            system("cls");
-            switch (menu) {
-                case 1: masuk();
-                        break;
-                default: main();
-            }
-        }
-        if(strcmp(datauser[i].arrusername,tryusername)==0){
-            if(strcmp(datauser[i].arrpassword,trypassword)==0){
-                usermenu();
-            }
-            else{
-                system("cls");
-                printf("(ops sorry Wrong Password)\n");
-                printf("1. Try again\n");
-                printf("2. Back to the Menu\n");
-                printf("\nSelect Menu: ");
-                scanf("%d",&menu);
-                fflush(stdin);
-                system("cls");
-                switch (menu) {
-                    case 1: masuk();
-                            break;
-                    default: main();
-                }
-            }
-        }
-        i++;
-    }
-}
-
-void daftar(){
-    char username[20];
-    char password[20];
-    FILE *datausername;
-    FILE *datapassword;
-
-    printf(" | = = = = = | Register | = = = = = | \n");
-    datausername=fopen("username.txt","a+");
-    printf("Enter username: ");
-    fgets(username,sizeof(username),stdin);
-    fputs(username,datausername);
-    fclose(datausername);
-
-    datapassword=fopen("password.txt","a+");
-    printf("Enter Password: ");
-    fgets(password,sizeof(password),stdin);
-    fputs(password,datapassword);
-    fclose(datapassword);
-    system("cls");
-
-    printf("(registered successfully)\n");
-}
-
-void usermenu(){
-    printf("Success entered user menu\n");
-    printf("1. Input Data Klub\n");
-    printf("\nSelect Menu: ");
-    scanf("%d",&menu);
-    fflush(stdin);
-    system("cls");
-    switch (menu) {
-        case 1: inputdata();break;
-        default: usermenu();
-    }
-}
-
-void inputdata(){
-    int i,n;
-    char nama[500];
-    int tropi,jumlahanggota;
-    char anggotaklub[500];
-    char negara[500];
-    char divisi[500];
-
-    FILE *dataklub;
-
-    dataklub=fopen("data_klub.txt","a");
-
-    if(dataklub==NULL){
-        printf("Data tidak ditemukan");
-    }
-
-    else{
-        printf("Masukkan Banyak Klub : ");
-        scanf("%d",&n);
-        for(i=0;i<n;i++){
-            fflush(stdin);
-            printf("Masukkan Nama Klub : ");
-            fgets(nama,sizeof(nama),stdin);
-            fflush(stdin);
-            fprintf(dataklub,"Nama Klub : %s",nama);
-            printf("Masukkan jumlah Tropi : ");
-            scanf("%d",&tropi);
-            fprintf(dataklub,"Jumlah Tropi : %d\n",tropi);
-            printf("Masukkan jumlah anggota : ");
-            scanf("%d",&jumlahanggota);
-            fprintf(dataklub,"Anggota :\n");
-            for(i=0;i<jumlahanggota;i++){
-                fflush(stdin);
-                printf("Masukkan Nama Anggota : ");
-                fgets(anggotaklub,sizeof(anggotaklub),stdin);
-                fprintf(dataklub,"%s",anggotaklub);
-            }
-            printf("Masukkan Negara Asal : ");
-            fgets(negara,sizeof(negara),stdin);
-            fflush(stdin);
-            fprintf(dataklub,"Negara Asal : %s",negara);
-            printf("Masukkan Divisi Klub : ");
-            fgets(divisi,sizeof(divisi),stdin);
-            fflush(stdin);
-            fprintf(dataklub,"Divisi : %s",divisi);
-        }
-        fclose(dataklub);
-    }
-}
-
-int main(){
-    while(1){
-        printf("| = = = = = | Welcome to Football Rank | = = = = = |\n");
-        printf("1. Log In");
-        printf("\n2. Sign up");
-        printf("\n3. Exit\n");
-        printf("\nSelect Menu: ");
-        scanf("%d",&menu);
-        fflush(stdin);
+    if(n == 1)
+    {
         system("cls");
-        switch (menu) {
-            case 1: masuk();
-                    break;
-            case 3: exit(0);
-            default: daftar();
+        createAccount();
+    }
+    else if(n == 2)
+    {
+        system("cls");
+        login();
+    }
+}
+
+void createAccount()
+{
+    FILE *account;
+
+    account=fopen("account.txt","w+");
+    if (account == NULL)
+    {
+        fputs("No such file exist!", stderr);
+        exit(1);
+    }
+    
+    struct userAccount userData;
+
+    printf("Register\n");
+    printf("Username: "); fgets(userData.username,50,stdin); 
+    printf("Password: "); fgets(userData.password,50,stdin); 
+    fwrite(&userData, sizeof(userData),1,account);
+    fclose(account);
+
+    system("cls");
+    main();
+}
+
+void login()
+{
+    char username[50], password[50];
+    FILE *data;
+
+    struct userAccount userData;
+
+    printf("Login\n");
+    printf("Username: "); fgets(username,50,stdin);
+    printf("Password: "); fgets(password,50,stdin);
+
+    data = fopen("account.txt","r");
+    if(data == NULL)
+    {
+        fputs("No such data exist", stderr);
+        createAccount();
+    }
+
+    while(fread(&userData, sizeof(userData),1,data));
+    {
+        if(strcmp(username, userData.username)==0 && strcmp(password, userData.password)==0)
+        {
+            printf("login success.\n");
+            menu();
+        }
+        else
+        {
+            printf("Your account is not registered.\n");
+            createAccount();
         }
     }
-    return 0;
 }
+
+void menu()
+{
+    system("cls");
+
+    int n;
+
+    printf("[Football Ranking Program]\n\n");
+    printf("Number of club: "); scanf("%d", &n); fflush(stdin);
+
+    struct club c[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d of %d\n", i+1, n);
+        printf("Name: "); fflush(stdin); fgets(c[i].name,50,stdin);
+        printf("Trophy: "); scanf("%d", &c[i].trophy); 
+    }
+
+    printf("\n[Your Data]\n");
+    for (int i = 0; i < n; i++)
+    {   
+        printf("[%d]\n", i+1);
+        printf("Name: %s\nTrophy: %d\n", c[i].name, c[i].trophy);
+    }
+
+    int x;
+    printf("\nData stored successfully.\n");
+    printf("Type '1' to start sorting...\n"); scanf("%d", &x);
+
+    if (x == 1)
+    {
+        int j;
+        struct club temp;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (j = 0; j < n-1-i; j++)
+            {
+                if (c[j].trophy < c[j+1].trophy)
+                {
+                    temp = c[j];
+                    c[j] = c[j+1];
+                    c[j+1] = temp;
+                }
+                
+            }
+            
+        }    
+    }
+    
+    printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("[%d]\n", i+1);
+        printf("Name: %s\nTrophy: %d\n", c[i].name, c[i].trophy);
+    }
+}
+
